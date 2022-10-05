f042e03392cf7296a094a949fa848de1b1d1e179
Abdumajid2012 <90825804+Abdumajid2012@users.noreply.github.com>
2022-10-05 20:37:05 +0500

    Teach Comparison

--- Program.cs
+++ Program.cs
@@ -12,3 +12,10 @@
         System.Console.WriteLine($"Bo`linma: {firstNumber / secondNumber}");
         System.Console.WriteLine($"Ko`paytma: {firstNumber * secondNumber}");
         System.Console.WriteLine($"Qoldiq: {firstNumber % secondNumber}" );
+
+        System.Console.WriteLine($"Birinchi son ikkinchi sondan katta:  {firstNumber > secondNumber}");
+        System.Console.WriteLine($"Birinchi son ikkinchi sondan kichik:  {firstNumber < secondNumber}");
+        System.Console.WriteLine($"Birinchi son ikkinchi sondan katta yoki teng:  {firstNumber >= secondNumber}");
+        System.Console.WriteLine($"Birinchi son ikkinchi sondan kichik yoki teng:  {firstNumber <= secondNumber}");
+        System.Console.WriteLine($"Birinchi son ikkinchi songa teng:  {firstNumber == secondNumber}");
+        System.Console.WriteLine($"Birinchi son ikkinchi songa tengemas:  {firstNumber != secondNumber}");
